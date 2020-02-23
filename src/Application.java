8a3c69246c92d52e6ad48d58e0a51b0976fec9ec
oksana <oksanaov@gmail.com>
2020-02-23 20:14:37 +0200

    exception in area method

--- src/Application.java
+++ src/Application.java
@@ -2,4 +2,11 @@ public class Application {
     public static double sqr(double arg){
         throw new RuntimeException();
     }
+
+    public static int area(int width, int height){
+        if (width<0 || height<0){
+            throw new IllegalArgumentException("Negative sizes: w = "+width+" h = "+height);
+        }
+        return width*height;
+    }
 }
