6a8963e03e56bc319592d0d5ded94ae2efe60e9f
oksana <oksanaov@gmail.com>
2020-02-24 03:21:16 +0200

    different examples added

--- src/Application.java
+++ src/Application.java
@@ -9,4 +9,30 @@ public class Application {
         }
         return width*height;
     }
+
+    public static void test1(){
+        try{
+            System.err.print(" 0");
+            if(true){throw new RuntimeException();}
+            System.err.print(" 1");
+        }catch(Exception e){
+            System.err.print(" 2");
+        }
+        System.err.print(" 3");
+    }
+
+    public static void test2(){
+        try{
+            System.err.println("try");
+        } finally {
+            System.err.println("finally");
+        }
+    }
+    public static void test3(){
+        try{
+            throw new RuntimeException();
+        } finally {
+            System.err.println("finally");
+        }
+    }
 }
